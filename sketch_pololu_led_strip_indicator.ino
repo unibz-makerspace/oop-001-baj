7582e2cfada0bf0a1204d02a38718ebccf7826cf
Julian Sanin <sanin89julian@gmail.com>
2015-07-10 11:47:19 +0200

    Added simple compass code

--- sketch_pololu_led_strip_indicator.ino
+++ sketch_pololu_led_strip_indicator.ino
@@ -1,11 +1,19 @@
 #include "Timer.h"
 #include "LedHelix.h"
 
+#include <SPI.h>
+#include <Wire.h>
+#include "Adafruit_LSM9DS0.h"
+
+#include <stdlib.h>
+#include <math.h>
+
 Timer timer;
 LedHelix ledHelix;
-//int angle = 0;
-int angle = 90;
-char c = '!';
+Adafruit_LSM9DS0 inertialModule = Adafruit_LSM9DS0();
+
+/*int angle = 90;
+char c = '!';*/
 
 static const unsigned char LED_ACTIVITY = 13;
 
@@ -13,12 +21,22 @@ void toggleLedActivity() {
   digitalWrite(LED_ACTIVITY, !digitalRead(LED_ACTIVITY));
 }
 
+unsigned int handleInertialModuleCompass() {
+  inertialModule.read();
+  double heading = 180.0*atan2(inertialModule.magData.x, inertialModule.magData.y)/3.14;
+  heading = (heading < 0) ? heading + 360.0 : heading;
+  return (unsigned int) heading;
+}
+
 void updatePololuLedStrip() {
-  /*ledHelix.clearColors();
+  /*
+  ledHelix.clearColors();
   ledHelix.pointCompassToDirection(angle);
   ledHelix.updateLeds();
   angle += 9;
-  angle %= 360;*/
+  angle %= 360;
+  */
+  /*
   ledHelix.clearColors();
   //ledHelix.drawCharacterAtDirectionWithColor(c, angle, (rgb_color){255,0,0});
   ledHelix.drawCharacterAtDirectionWithColor('W', angle, (rgb_color){255,0,0});
@@ -32,6 +50,19 @@ void updatePololuLedStrip() {
     }
   }
   angle %= 360;
+  */
+  ledHelix.clearColors();
+  ledHelix.pointCompassToDirection(handleInertialModuleCompass());
+  ledHelix.updateLeds();
+}
+
+void setupInertialModule() {
+  if(inertialModule.begin()) {
+    inertialModule.setupMag(inertialModule.LSM9DS0_MAGGAIN_12GAUSS);
+    inertialModule.setupAccel(inertialModule.LSM9DS0_ACCELRANGE_16G);
+  } else {
+    abort();
+  }
 }
 
 void setup() {
@@ -42,7 +73,9 @@ void setup() {
   pinMode(LED_ACTIVITY, OUTPUT);
   
   timer.setInterval(toggleLedActivity, 1000);
-  timer.setInterval(updatePololuLedStrip, 40); // 25fps.
+  timer.setInterval(updatePololuLedStrip, 250); // 25fps.
+
+  setupInertialModule();
 }
 
 void loop() {
